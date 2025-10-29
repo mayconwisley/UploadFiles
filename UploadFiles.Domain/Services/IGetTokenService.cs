cee08d82a3b1926915c6cf921fc73ab3d467946f
Maycon Wisley <maycon.wisley@outlook.com>
2025-10-29 16:26:51 -0300

    Ajuste

---
+++ UploadFiles.Domain/Services/IGetTokenService.cs
@@ -0,0 +1,9 @@
+﻿using UploadFiles.Domain.Abstractions;
+using UploadFiles.Domain.Entities;
+
+namespace UploadFiles.Domain.Services;
+
+public interface IGetTokenService
+{
+    public Task<Result<string>> Token(User user);
+}
